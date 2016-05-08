36afc2831b1b4772eecb703208da0899e71880b4
koorchik <koorchik@gmail.com>
2016-05-08 06:57:44 +0300

    Download large logos too

--- bin/crawl_logos.pl
+++ bin/crawl_logos.pl
@@ -4,11 +4,12 @@ use feature qw/signatures/;
 no warnings qw/experimental::signatures/;
 
 use FindBin qw/$Bin/;
-use JSON qw/encode_json/;
+use JSON;
 use Path::Tiny;
 use Mojo::UserAgent;
 
-my $UA = Mojo::UserAgent->new();
+my $UA = Mojo::UserAgent->new;
+my $JSON = JSON->new->utf8->pretty;
 
 main();
 
@@ -19,7 +20,7 @@ sub main {
 
     foreach my $letter ('A'..'Z') {
         my $page_url = "$host/Tags-$letter/";
-        my @infos = get_logos_info_from_page($page_url);
+        my @infos = get_logos_info_from_page($page_url, $host);
 
         foreach my $logo_info (@infos) {
             download_logo_data($logo_info, $results_dir);
@@ -27,7 +28,7 @@ sub main {
     }
 }
 
-sub get_logos_info_from_page($url) {
+sub get_logos_info_from_page($url, $host) {
     say "GET $url";
 
     my $clean = sub($str) {
@@ -35,10 +36,23 @@ sub get_logos_info_from_page($url) {
        return $str;
     };
 
-    return $UA->get($url)->res->dom->find('.logobox dt')->map(sub {{
-       img_src => $_->at('img')->attr('src'),
-       name    => $clean->( $_->find('a')->[1]->content )
-    }})->each;
+    return $UA->get($url)->res->dom->find('.logobox dt')->map(sub {
+        my $link = $_->find('a')->[1];
+        my $link_href = $link->attr('href');
+
+        say "GET $link_href";
+        my $large_img_src = $UA->get( $link_href )->res->dom
+           ->at('.content')->at('img')->attr('src');
+
+        $large_img_src = "$host/$large_img_src" unless $large_img_src =~ /^http/;
+
+        return {
+           img_src       => $_->at('img')->attr('src'),
+           large_img_src => $large_img_src,
+           details_url   => $link_href,
+           name          => $clean->( $link->content )
+        };
+    })->each;
 }
 
 sub download_logo_data($logo_info, $results_dir) {
@@ -48,10 +62,19 @@ sub download_logo_data($logo_info, $results_dir) {
     path($dst_dir)->mkpath() unless -e $dst_dir;
 
     # Save metadata
-    path("$dst_dir/meta.json")->spew( encode_json($logo_info) );
+    path("$dst_dir/meta.json")->spew( $JSON->encode($logo_info) );
 
     # Download image
+    say "  Download $logo_info->{img_src}";
     $UA->get( $logo_info->{img_src} )
        ->res->content->asset
        ->move_to("$dst_dir/image.jpg");
+
+     # Download large image
+     say "  Download $logo_info->{large_img_src}";
+     $UA->get( $logo_info->{large_img_src} )
+        ->res->content->asset
+        ->move_to("$dst_dir/image_large.jpg");
+
+     say "---";
 }
